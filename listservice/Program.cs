702daa3b86ce241babc80413b0ba0c3b8f1821ab
eaa24 <eaa24imha@students.eaaa.dk>
2026-03-05 23:47:47 +0100

    Add ListImagesService with Dockerfile and listImages endpoint

--- listservice/Program.cs
+++ listservice/Program.cs
@@ -1,23 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Kestrel lytter på 8080 (container)
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Kun HTTPS-redirect i udvikling, ikke i container
 if (app.Environment.IsDevelopment())
 {
+    app.UseHttpsRedirection();
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
--- listservice/obj/Release/net10.0/listservice.AssemblyInfo.cs
+++ listservice/obj/Release/net10.0/listservice.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("listservice")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa8f1a7de63f4ec70bca8133f148ca7cb923848c")]
 [assembly: System.Reflection.AssemblyProductAttribute("listservice")]
